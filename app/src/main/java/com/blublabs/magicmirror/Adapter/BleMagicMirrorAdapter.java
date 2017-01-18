8a0e68625c136da8037f756bd4437dc3cda12561
Andreas <andr.schulz90@gmail.com>
2017-01-18 21:28:09 +0100

    Compile Fix

--- app/src/main/java/com/blublabs/magicmirror/Adapter/BleMagicMirrorAdapter.java
+++ app/src/main/java/com/blublabs/magicmirror/Adapter/BleMagicMirrorAdapter.java
@@ -1,6 +1,5 @@
 package com.blublabs.magicmirror.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
 import android.support.annotation.NonNull;
--- app/src/main/java/com/blublabs/magicmirror/Adapter/IMagicMirrorAdapter.java
+++ app/src/main/java/com/blublabs/magicmirror/Adapter/IMagicMirrorAdapter.java
@@ -1,6 +1,5 @@
 package com.blublabs.magicmirror.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
